9358b16a63085315b3d2414c6f2373d77607cd16
hieuhn <hieungochoangit@gmail.com>
2022-11-06 00:57:28 +0700

    Created base folder

---
+++ src/routers/admin/admin-router.ts
@@ -0,0 +1,8 @@
+import express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("admin router");
+});
+
+module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
-});
+// Admin router
+const adminRouter = require("./routers/admin/admin-router");
+app.use("/admin", adminRouter);
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
