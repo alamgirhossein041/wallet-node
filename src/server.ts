2351202bde071054cc735b36f9b1555959f08a06
hieuhn <hieungochoangit@gmail.com>
2022-11-06 00:38:45 +0700

    Init project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express = require("express");
+import dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at https://localhost:${port}`);
+});
